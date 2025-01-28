99efb4e81891203acd6df926bd419d7b1512f3c4
Mahnoor <mano.sheikh16@yahoo.com>
2025-01-28 02:32:03 -0500

    commit for model 2

--- HW1/script.py
+++ HW1/script.py
@@ -18,28 +18,47 @@ pd.concat([total, percent], axis=1, keys=['Total', 'Percent']).transpose()
 
 #find best fitting X
 print(data.corr())
+# x4 has the highest absolute correlation with y, followed by x2 
 
+## Q1-b
 #split test and train data
 x = data[['x4']]
 y = data[['y']]
 
-#will train on 70% of the data, test on the remaining 30%
+#train on 70% of the data, test on the remaining 30%
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=50)
 lr = LinearRegression()
 lr.fit(x_train, y_train)
 y_pred = lr.predict(x_test)
 
-print("MSE =", mean_squared_error(y_test, y_pred))
-print("R^2 Score =", r2_score(y_test, y_pred))
-
+#results
+print("MSE model 1 =", mean_squared_error(y_test, y_pred))
+print("R^2 Score model 1 =", r2_score(y_test, y_pred))
 # MSE = 85.07018698169895
 # R^2 Score = 0.2752714488833352
 
 #organize work
 os.makedirs("HW1/model", exist_ok=True)
 joblib.dump(lr, "HW1/model/lr_model1.pkl")
-
-x_train.to_csv("HW1/data/x_train.csv", index=False)
-x_test.to_csv("HW1/data/x_test.csv", index=False)
+x_train.to_csv("HW1/data/x_train1.csv", index=False)
+x_test.to_csv("HW1/data/x_test1.csv", index=False)
 y_train.to_csv("HW1/data/y_train.csv", index=False)
 y_test.to_csv("HW1/data/y_test.csv", index=False)
+
+## Q1-c
+x = data[['x2', 'x4']]
+y = data[['y']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=50)
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+y_pred = lr.predict(x_test)
+
+print("MSE model 2 =", mean_squared_error(y_test, y_pred))
+print("R^2 Score model 2 =", r2_score(y_test, y_pred))
+# MSE = 56.49218958495245
+# R^2 Score = 0.5187326587618971
+
+joblib.dump(lr, "HW1/model/lr_model2.pkl")
+x_train.to_csv("HW1/data/x_train2.csv", index=False)
+x_test.to_csv("HW1/data/x_test2.csv", index=False)
